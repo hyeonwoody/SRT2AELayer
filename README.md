a3f8632268c04558b577b370a276f4a4c9b0352a
root <hyeonwoody@gmail.com>
2023-03-13 21:12:51 +0900

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# BubblePop
+.srt 자막 파일을 Adobe After Effect의 Timeline Layer로 변환해주는 소스코드.
+
+</br>
+
+## 🧑‍💻: Intro
+ 영상 편집의 자막 작업은 단순 반복을 요구하는데, 
+ 이를 Adobe ExtendScript을 활용한 자동화를 통 
+ 작업 시간을 줄일 수 있습니다.
+
+</br>
+
+## 📞: Contact
+- 이메일: hyeonwoody@gmail.com
+- 블로그: https://velog.io/@hyeonwoody
+- 깃헙: https://github.com/hyeonwoody
+
+</br>
+
+## 🧱: Technologies Used
+>JavaScript (Executed at Runtime rather than being compiled)
+>ExtendScript Toolkit CC (IDE)
+
+## 📖: Libraries Used
+>ExtendScript Toolkit CC
---
+++ srt2AELayer.jsx
@@ -0,0 +1,125 @@
+﻿
+
+var comp = app.project.activeItem;
+
+var lineNumber;
+
+var number;
+var startTime;
+var endTime;
+var comment;
+
+
+
+function initialize(){
+    text = new File("/"); //path to the .srt file
+    text.open('r');
+    
+    lineNumber =69; //Total subtitle Number
+    
+    
+}
+
+function readTextFile(){
+    var i =0;
+    
+    number = new Array(lineNumber);
+    startTime = new Array(lineNumber);
+    endTime = new Array(lineNumber);
+    comment = new Array(lineNumber);
+    
+    var time;
+    var tmp;
+    while (i<lineNumber){
+        number[i] = text.readln();
+        
+        time=text.readln();
+        tmp=time.substring(0,12).split(":");
+        startTime[i] = (parseInt(tmp[0], 10) *60*60) + (parseInt(tmp[1],10) *60)+(parseInt(tmp[2],10)*0.001);
+        
+        tmp = time.substring(17).split(":");
+        endTime[i] = (parseInt(tmp[0], 10) *60*60) + (parseInt(tmp[1],10) *60)+(parseInt(tmp[2],10)*0.001);
+        
+        comment[i]=text.readln();
+        
+        text.readln();
+    
+    $.writeln("number : "+number[i]);
+    $.writeln("time : "+time);
+    $.writeln("time : "+startTime[i]+" --> "+endTime[i]);
+    $.writeln("commnet : "+comment[i]);
+    if (comment[i] [0]!= "!" && comment[i] [0]!= "@" && comment[i] [0]!= "#"){  i++;}
+      //if (comment[i][0]== "#")  { comment[i]=comment[i].slice(1);i++;}
+      }
+}                                                      
+
+function basic(){
+    
+   var i=0;
+   var textProp;
+var textDocument;   
+    
+    var normal=1;
+    var impact =0;
+   if (normal ==1){
+       while (i<lineNumber){
+            newLayer = comp.layers.addText(comment[i]);
+            newLayer.position.setValue([959.893,952]);
+            
+            textProp = newLayer.property("Source Text");
+            textDocument = textProp.value;
+            
+            textDocument.fontSize =56;
+            textDocument.font = "Cafe24 Dangdanghae";
+            textDocument.fillColor = [0,0,0];
+            textDocument.applyFill = true;
+            textDocument.justification = ParagraphJustification.CENTER_JUSTIFY;
+            textProp.setValue(textDocument);
+           
+            newLayer.inPoint = startTime[i];
+            newLayer.outPoint = endTime[i];
+            
+            i++;
+        
+        }
+    }
+    if (impact==1){
+        while (i<lineNumber){
+            newLayer = comp.layers.addText(comment[i]);
+            newLayer.position.setValue([959.893,952]);
+            
+            textProp = newLayer.property("Source Text");
+            textDocument = textProp.value;
+            
+            textDocument.fontSize =100;
+            textDocument.font = "HYPMokGak-Bold";
+            textDocument.fillColor = [0.192,0.67,0.80392];
+            textDocument.strokeColor = [1, 1, 1];
+            textDocument.strokeWidth = 23;
+      
+            textDocument.applyFill = true;
+            textDocument.applyStroke = true;
+            textDocument.justification = ParagraphJustification.CENTER_JUSTIFY;
+            textProp.setValue(textDocument);
+           
+            newLayer.inPoint = startTime[i];
+            newLayer.outPoint = endTime[i];
+            
+            i++;
+        
+        }
+    }
+}
+
+
+initialize();
+readTextFile();
+basic();
+print();
+
+
+function print(){
+     $.writeln("print : "+comment[455]);
+     
+      $.writeln("start 301 : "+startTime[300]);
+     }
